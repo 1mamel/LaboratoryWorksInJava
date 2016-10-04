f976d418352d82e53ebe550f680d24a6c87488f8
Kamil Pilch <mamlikk2@gmail.com>
2016-10-04 14:20:19 +0200

    poprawiony

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,4 @@
 package com.company;
-
-import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -9,12 +7,54 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Witamy w moim świetnym programi do obliczeń");
-        System.out.println("Podaj N:");
-        int N = Integer.parseInt(scanner.nextLine());
-        System.out.println("Podaj MIN:");
-        int MIN = Integer.parseInt(scanner.nextLine());
-        System.out.println("Podaj MAX:");
-        int MAX = Integer.parseInt(scanner.nextLine()) * 10;
-        System.out.println("wynik randomizacji:" + (ThreadLocalRandom.current().nextInt(MIN, MAX)));
+        int N = 0;
+        int MIN = 0;
+        int MAX = 0;
+        while(true) {
+            try{
+                System.out.println("Podaj N:");
+                N = Integer.parseInt(scanner.nextLine());
+                break;
+            }
+            catch(Exception e) {
+                System.out.println("Nie podano liczby");
+            }
+        }
+        while(true) {
+            try{
+                System.out.println("Podaj MIN:");
+                MIN = Integer.parseInt(scanner.nextLine());
+                break;
+            }
+            catch(Exception e) {
+                System.out.println("Nie podano liczby");
+            }
+        }
+        while(true) {
+            try{
+                System.out.println("Podaj MAX:");
+                MAX = Integer.parseInt(scanner.nextLine());
+                if(MAX <= MIN)
+                {
+                    System.out.println("Max nie moze byc mniejszy badz rowny MIN:");
+                    continue;
+                }
+                break;
+            }
+            catch(Exception e) {
+                System.out.println("Nie podano liczby");
+            }
+        }
+        int iloscTrafien = 0;
+
+        for (int i = 0;i<N;i++)
+        {
+            int wylosowany = ThreadLocalRandom.current().nextInt(MIN, MAX*10);
+            if(wylosowany <= MAX && wylosowany >= MIN)
+            {
+                iloscTrafien++;
+            }
+        }
+            System.out.println("Ilosc trafionych:" + iloscTrafien);
     }
 }
